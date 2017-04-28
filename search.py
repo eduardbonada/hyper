36d4d6ebaea68f3c41bae5af1c96a525c4b842cf
Eduard Bonada <eduard.bonada@gmail.com>
2017-04-28 19:55:40 +0200

    Search script storing into DB

--- search.py
+++ search.py
@@ -11,38 +11,6 @@ access_secret = '4AFqod7kCScnSDf9OcgmVeIdnxwa9ZKn9pwwFMBbpLi7u'
 # Setup sqlite
 sqlite_file = 'hyper.db'
 
-# Connect to the database sqlite file
-connection = sqlite3.connect(sqlite_file)
-db = connection.cursor()
-
-query = 'python'
-max_tweets = 10
-searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
-
-
-# Store the tweet in DB
-try:
-    db.execute("INSERT INTO TweetsRaw (tweetId,createdAt,tweetText,favsCount,rtsCount,language,userFriendsCount,userFollowersCount,userStatusesCount,userFavsCount,userLocation) \
-                VALUES ('{tweetId}','{createdAt}','{tweetText}','{favsCount}','{rtsCount}','{language}','{userFriendsCount}','{userFollowersCount}','{userStatusesCount}','{userFavsCount}','{userLocation}')".format(\
-                    tweetId=tweet_info['id_str'], \
-                    createdAt=tweet_info['created_at'], \
-                    tweetText=tweet_info['text'], \
-                    favsCount=tweet_info['favorite_count'], \
-                    rtsCount=tweet_info['retweet_count'], \
-                    language=tweet_info['lang'], \
-                    userFriendsCount=tweet_info['user']['friends_count'], \
-                    userFollowersCount=tweet_info['user']['followers_count'], \
-                    userStatusesCount=tweet_info['user']['statuses_count'], \
-                    userFavsCount=tweet_info['user']['favourites_count'], \
-                    userLocation=tweet_info['user']['location']) \
-    )
-except sqlite3.Error as e:
-    print("Error: ", e)
-
-# Commit and close
-connection.commit()
-connection.close()
-    
 # Manage twitter API access
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
@@ -50,8 +18,42 @@ auth.set_access_token(access_token, access_secret)
 # Create tweepy instance
 api = tweepy.API(auth)
 
-# Setup streaming
-twitter_stream = tweepy.Stream(auth, TweetsListener())
+# Connect to the database sqlite file
+connection = sqlite3.connect(sqlite_file)
+db = connection.cursor()
 
-# Launch streaming
-twitter_stream.filter(track=['#trump'])
+# search tweets
+query = 'primavera sound'
+max_tweets = 100
+searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+
+print("Found {} tweets about '{}'".format(len(searched_tweets), query))
+
+# Store the tweets in DB
+for st in searched_tweets:
+    try:
+        tweet_info = st._json;
+
+        print("Inserting tweet {} => {}, {}".format(tweet_info['id_str'],tweet_info['text'],tweet_info['user']['location']))
+
+        db.execute("INSERT INTO TweetsRaw (tweetId,createdAt,tweetText,favsCount,rtsCount,language,userFriendsCount,userFollowersCount,userStatusesCount,userFavsCount,userLocation) \
+                    VALUES ('{tweetId}','{createdAt}','{tweetText}','{favsCount}','{rtsCount}','{language}','{userFriendsCount}','{userFollowersCount}','{userStatusesCount}','{userFavsCount}','{userLocation}')".format(\
+                        tweetId=tweet_info['id_str'], \
+                        createdAt=tweet_info['created_at'], \
+                        tweetText=tweet_info['text'].replace("'","''"), \
+                        favsCount=tweet_info['favorite_count'], \
+                        rtsCount=tweet_info['retweet_count'], \
+                        language=tweet_info['lang'], \
+                        userFriendsCount=tweet_info['user']['friends_count'], \
+                        userFollowersCount=tweet_info['user']['followers_count'], \
+                        userStatusesCount=tweet_info['user']['statuses_count'], \
+                        userFavsCount=tweet_info['user']['favourites_count'], \
+                        userLocation=tweet_info['user']['location']) \
+        )
+    except sqlite3.Error as e:
+        print("Error: ", e)
+
+# Commit and close
+connection.commit()
+connection.close()
+    
