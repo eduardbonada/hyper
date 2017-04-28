ecc9b305cb38008f11d042f75ce09b62807583cc
Eduard Bonada <eduard.bonada@gmail.com>
2017-04-28 19:10:37 +0200

    Completed streaming tweets and storing in sqlite db

---
+++ streaming.py
@@ -0,0 +1,82 @@
+# https://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
+# http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+# https://www.dataquest.io/blog/streaming-data-python/
+# http://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html
+
+import tweepy
+import sqlite3
+from pprint import pprint
+import json
+
+# Setup twitter API access
+consumer_key = 'Ib3yDL5HYSLxAqENZ6QCHRFex'
+consumer_secret = 'TuTQKld9os111vx7oMSM3PTfoNz9dZDcnACxIvHGL9euIvLE8I'
+access_token = '74265344-UOJgWD9vzB9wJvgnet3f63bkQdJ0rLGz9gg67fqDP'
+access_secret = '4AFqod7kCScnSDf9OcgmVeIdnxwa9ZKn9pwwFMBbpLi7u'
+
+# Setup sqlite
+sqlite_file = 'hyper.db'
+
+# Class that manages the events received from streaming API
+class TweetsListener(tweepy.StreamListener):
+ 
+    def on_status(self, status):
+        """ Manage 'status' event."""
+
+        tweet_info = status._json;
+
+        # print the tweet in console
+        # pprint(tweet_info)
+        # print(tweet_info['id_str'])
+
+        # Connect to the database sqlite file
+        connection = sqlite3.connect(sqlite_file)
+        db = connection.cursor()
+
+        print("Trying to save tweet {} info into DB".format(tweet_info['id_str']))
+
+        # Store the tweet in DB
+        try:
+            db.execute("INSERT INTO TweetsRaw (tweetId,createdAt,tweetText,favsCount,rtsCount,language,userFriendsCount,userFollowersCount,userStatusesCount,userFavsCount,userLocation) \
+                        VALUES ('{tweetId}','{createdAt}','{tweetText}','{favsCount}','{rtsCount}','{language}','{userFriendsCount}','{userFollowersCount}','{userStatusesCount}','{userFavsCount}','{userLocation}')".format(\
+                            tweetId=tweet_info['id_str'], \
+                            createdAt=tweet_info['created_at'], \
+                            tweetText=tweet_info['text'], \
+                            favsCount=tweet_info['favorite_count'], \
+                            rtsCount=tweet_info['retweet_count'], \
+                            language=tweet_info['lang'], \
+                            userFriendsCount=tweet_info['user']['friends_count'], \
+                            userFollowersCount=tweet_info['user']['followers_count'], \
+                            userStatusesCount=tweet_info['user']['statuses_count'], \
+                            userFavsCount=tweet_info['user']['favourites_count'], \
+                            userLocation=tweet_info['user']['location']) \
+            )
+        except sqlite3.Error as e:
+            print("Error: ", e)
+
+        # Commit and close
+        connection.commit()
+        connection.close()
+        
+        return True
+ 
+    def on_error(self, status):
+        """ Manage 'error' event."""
+
+        print("Error TweetsListener:on_error => %s" % str(status))
+        return True
+
+if __name__ == '__main__':
+    
+    # Manage twitter API access
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+
+    # Create tweepy instance
+    api = tweepy.API(auth)
+
+    # Setup streaming
+    twitter_stream = tweepy.Stream(auth, TweetsListener())
+
+    # Launch streaming
+    twitter_stream.filter(track=['#python'])
