4b9433942eb1a8812dea800b11842e058d5a7707
Eduard Bonada <eduard.bonada@gmail.com>
2017-05-27 07:41:30 +0200

    add helpers script

---
+++ helpers.py
@@ -0,0 +1,71 @@
+
+"""
+Create Ranking
+"""
+
+def extract_bands(tweet):
+    """
+    Function that extracts the bands from a tweet text
+    Returns a list of bands
+    """
+    
+    # init list to return
+    bands_in_tweet = []
+    
+    # loop all bands and check of any of the written forms is present in the tweet text
+    for i, b in bands.iterrows():
+                
+        # set different band names writing possibilities
+        bandname = b['name']
+        bandname_lowercase = bandname.lower()
+        bandname_lowercase_no_spaces = ''.join(bandname_lowercase.split())
+        bandname_lowercase_no_accents = ''.join((c for c in unicodedata.normalize('NFD', bandname_lowercase) if unicodedata.category(c) != 'Mn'))
+        bandname_lowercase_no_spaces_no_accents = ''.join((c for c in unicodedata.normalize('NFD', bandname_lowercase_no_spaces) if unicodedata.category(c) != 'Mn'))
+
+        # check if any of the forms is in the tweet text
+        if any(s in tweet['tweetText'].lower() for s in [bandname_lowercase, bandname_lowercase_no_spaces, bandname_lowercase_no_accents, bandname_lowercase_no_spaces_no_accents, b['twitterName']]):
+            bands_in_tweet.append({"id": b['id'], "codedName": b['codedName']})
+
+    return bands_in_tweet
+
+
+def band_partition(tweet):
+    """
+    Function that reads a single tweet info and adds into a list the tweet information partitioned by bands.
+    I.e. If a tweet mentions 2 bands, it adds a list of 2 dicts with the tweet info
+    """
+    
+    # loop all bands and add an entry to the list
+    for b in tweet['bands']:
+        new_band_tweets_list.append({\
+                                 "tweetRawId" : tweet['id'],\
+                                 "createdAt" : tweet['createdAt'],\
+                                 "storedAt" : tweet['storedAt'],\
+                                 "bandId" : b['id'],\
+                                 "bandCodedName" : b['codedName'],\
+                                 "favsCount" : tweet['favsCount'],\
+                                 "rtsCount" : tweet['rtsCount'],\
+                                 "language" : tweet['language'],\
+                                 "userId" : tweet['userId'],\
+                                 "userFriendsCount" : tweet['userFriendsCount'],\
+                                 "userFollowersCount" : tweet['userFollowersCount'],\
+                                 "userStatusesCount" : tweet['userStatusesCount'],\
+                                 "userFavsCount" : tweet['userFavsCount'],\
+                                 "userLocation" : tweet['userLocation']\
+                                })
+    
+    # Mark TweetsRaw as processed
+    db.execute("UPDATE TweetsRaw SET processed = 1 WHERE id == {}".format(tweet.id))
+    connection.commit()
+
+def compareBandPosition(band_row):
+    """
+    Function that compares the position of a band in the current ranking (in band_row) compared to the
+    position in the last ranking
+    """
+    new_position = band_row.ranking_position
+    difference = 0
+    if(any(last_ranking['bandId'].isin([band_row['bandId']]))):
+        last_postion = last_ranking['ranking_position'][last_ranking.bandId == band_row.bandId].values[0]
+        difference = last_postion - new_position
+    return difference
