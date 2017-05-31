39209c4dfa59856270d096cd1a4d91020ec38b82
Eduard Bonada <eduard.bonada@gmail.com>
2017-05-31 17:25:45 +0200

    reduce length of monitoring logs

--- monitoring.py
+++ monitoring.py
@@ -33,8 +33,8 @@ print("<html><body><pre>");
 ranking = pd.read_sql_query("SELECT * FROM BandsHype AS bh LEFT JOIN Bands AS b ON bh.bandId = b.id", connection)
 ranking = ranking.rename(columns={'tweets':'tw', 'favs':'fv', 'retweets':'rt','ranking_position':'pos', 'ranking_change':'chng', 'trending_level':'trend'})
 ranking['bfibp'] = ranking['bf_ibp'].round(2)
-print("\n\n********************\nTOP RANKED\n\n{}".format(ranking.sort_values('bf_ibp', ascending=False).head(10)[['name','tw','fv','rt','bfibp','pos','chng','trend']]))
 print("\n\n********************\nTOP TRENDING\n\n{}".format(ranking.sort_values('trend', ascending=False).head(10)[['name','tw','fv','rt','bfibp','pos','chng','trend']]))
+print("\n\n********************\nTOP RANKED\n\n{}".format(ranking.sort_values('bf_ibp', ascending=False).head(10)[['name','tw','fv','rt','bfibp','pos','chng','trend']]))
 
 
 # Number of tweets in db
@@ -48,13 +48,13 @@ print("    Band Tweets: {}".format(num_band_tweets))
 
 
 # List of last N tweets
-tweetsRaw = pd.read_sql_query("SELECT * FROM TweetsRaw ORDER BY id DESC LIMIT 10", connection)
+tweetsRaw = pd.read_sql_query("SELECT * FROM TweetsRaw ORDER BY id DESC LIMIT 3", connection)
 tweetsRaw['createdAt'] = pd.to_datetime(tweetsRaw['createdAt'], format ='%a %b %d %H:%M:%S +0000 %Y').dt.strftime('%d %H:%M')
 print("\n\n********************\nLAST TWEETS\n\n{}".format(tweetsRaw[['createdAt', 'tweetText']].values))
 
 
 # List of last N band tweets
-bandTweets = pd.read_sql_query("SELECT * FROM BandTweets AS bt LEFT JOIN TweetsRaw AS tr ON bt.tweetRawId = tr.id ORDER BY tr.id DESC LIMIT 10", connection)
+bandTweets = pd.read_sql_query("SELECT * FROM BandTweets AS bt LEFT JOIN TweetsRaw AS tr ON bt.tweetRawId = tr.id ORDER BY tr.id DESC LIMIT 3", connection)
 bandTweets['createdAt'] = pd.to_datetime(bandTweets['createdAt'], format ='%a %b %d %H:%M:%S +0000 %Y').dt.strftime('%d %H:%M')
 print("\n\n********************\nLAST BAND TWEETS\n\n{}".format(bandTweets[['createdAt', 'tweetText']].values))
 
@@ -64,7 +64,7 @@ try:
 	print("\n\n********************\nSTREAMING LOG\n")
 	with open(cron_streaming_file) as fid:
 	    lines = fid.readlines()
-	for line in lines[len(lines)-10:]:
+	for line in lines[len(lines)-5:]:
 	    print(line, end='')
 except:
 	print('ERROR: Streaming log file not found')
